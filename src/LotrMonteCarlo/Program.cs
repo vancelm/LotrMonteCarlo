b18f52ed9af35adab15a5058f2123e522ddc8446
Vance Morgan <vance@vancemorgan.com>
2024-04-15 16:32:31 -0500

    Crete "Leg One"

--- src/LotrMonteCarlo/Program.cs
+++ src/LotrMonteCarlo/Program.cs
@@ -1,10 +1,22 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 namespace LotrMonteCarlo;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<JourneyLeg> legs = new()
+        {
+            new("Leg One")
+            {
+                new Encounter("Hobbit", 0.5,  1,    0),
+                new Encounter("Elf",    0.22, 0.98, 0.02),
+                new Encounter("Human",  0.21, 0.95, 0.05),
+                new Encounter("Dwarf",  0.06, 0.99, 0.01),
+                new Encounter("Nazgul", 0.01, 0.35, 0.65),
+            },
+        };
     }
 }
