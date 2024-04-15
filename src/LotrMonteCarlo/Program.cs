bc2c78118622431006e4c29f959d7a301cd0e90d
Vance Morgan <vance@vancemorgan.com>
2024-04-15 16:43:32 -0500

    Added "Leg Three"

--- src/LotrMonteCarlo/Program.cs
+++ src/LotrMonteCarlo/Program.cs
@@ -19,7 +19,7 @@ internal class Program
             },
             new("Leg Two")
             {
-                new Encounter("Elf",    0.45, 1.00, 0),
+                new Encounter("Elf",    0.45, 1,    0),
                 new Encounter("Dwarf",  0.22, 0.99, 0.01),
                 new Encounter("Human",  0.15, 0.95, 0.05),
                 new Encounter("Orc",    0.07, 0.55, 0.45),
@@ -27,6 +27,16 @@ internal class Program
                 new Encounter("Troll",  0.03, 0.45, 0.55),
                 new Encounter("Balrog", 1,    0.1,  0.90),
             },
+            new("Leg Three")
+            {
+                new Encounter("Elf",      0.2,   1,    0),
+                new Encounter("Uruk-Hai", 0.17,  0.4,  0.6),
+                new Encounter("Goblin",   0.14,  0.5,  0.5),
+                new Encounter("Human",    0.11,  0.95, 0.05),
+                new Encounter("Dwarf",    0.105, 0.99, 0.01),
+                new Encounter("Orc",      0.095, 0.45, 0.55),
+                new Encounter("Nazgul",   0.08,  0.2,  0.80),
+            }
         };
     }
 }
