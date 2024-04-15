7d3f50460fad8de35105b97d7e1b03ffb630987a
Vance Morgan <vance@vancemorgan.com>
2024-04-15 16:39:47 -0500

    Added "Leg Two"

--- src/LotrMonteCarlo/Program.cs
+++ src/LotrMonteCarlo/Program.cs
@@ -17,6 +17,16 @@ internal class Program
                 new Encounter("Dwarf",  0.06, 0.99, 0.01),
                 new Encounter("Nazgul", 0.01, 0.35, 0.65),
             },
+            new("Leg Two")
+            {
+                new Encounter("Elf",    0.45, 1.00, 0),
+                new Encounter("Dwarf",  0.22, 0.99, 0.01),
+                new Encounter("Human",  0.15, 0.95, 0.05),
+                new Encounter("Orc",    0.07, 0.55, 0.45),
+                new Encounter("Goblin", 0.07, 0.5,  0.5),
+                new Encounter("Troll",  0.03, 0.45, 0.55),
+                new Encounter("Balrog", 1,    0.1,  0.90),
+            },
         };
     }
 }
