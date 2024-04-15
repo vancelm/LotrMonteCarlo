078240f68e3ca4dfcf40e080816547fcea149237
Vance Morgan <vance@vancemorgan.com>
2024-04-15 16:32:03 -0500

    Added Encounter.cs

---
+++ src/LotrMonteCarlo/Encounter.cs
@@ -0,0 +1,9 @@
+﻿namespace LotrMonteCarlo;
+
+public class Encounter(string name, double encounterProbability, double surviveProbability, double dieProbability)
+{
+    public string Name { get; } = name;
+    public double EncounterProbability { get; } = encounterProbability;
+    public double SurviveProbability { get; } = surviveProbability;
+    public double Die { get; } = dieProbability;
+}
