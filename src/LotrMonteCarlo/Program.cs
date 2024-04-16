3571def728b8da7474b43affc08d006ac249839c
Vance Morgan <vance@vancemorgan.com>
2024-04-15 20:10:06 -0500

    Cleaned up

--- src/LotrMonteCarlo/Program.cs
+++ src/LotrMonteCarlo/Program.cs
@@ -44,12 +44,12 @@ internal class Program
             }
         }
 
-        double successPercent = ((double)successCount) / iterations * 100;
+        double successPercent = ((double)successCount) / iterations;
         int failCount = iterations - successCount;
-        double failPercent = ((double)failCount) / iterations * 100;
+        double failPercent = ((double)failCount) / iterations;
 
-        Console.WriteLine($"Success: {successCount} ({successPercent}%)");
-        Console.WriteLine($"Fail: {failCount} ({failPercent}%)");
+        Console.WriteLine($"Success: {successCount} ({successPercent:0.00%})");
+        Console.WriteLine($"Fail: {failCount} ({failPercent:0.00%})");
     }
 
     private static (string Encounter, bool Survived) RollDice(JourneyLeg leg)
@@ -74,41 +74,41 @@ internal class Program
     {
         new("Leg One")
         {
-            new Encounter("Hobbit", 0.5,  1),
-            new Encounter("Elf",    0.22, 0.98),
-            new Encounter("Human",  0.21, 0.95),
-            new Encounter("Dwarf",  0.06, 0.99),
-            new Encounter("Nazgul", 0.01, 0.35),
+            new("Hobbit", 0.5,  1),
+            new("Elf",    0.22, 0.98),
+            new("Human",  0.21, 0.95),
+            new("Dwarf",  0.06, 0.99),
+            new("Nazgul", 0.01, 0.35),
         },
         new("Leg Two")
         {
-            new Encounter("Elf",    0.45, 1),
-            new Encounter("Dwarf",  0.22, 0.99),
-            new Encounter("Human",  0.15, 0.95),
-            new Encounter("Orc",    0.07, 0.55),
-            new Encounter("Goblin", 0.07, 0.5),
-            new Encounter("Troll",  0.03, 0.45),
-            new Encounter("Balrog", 1,    0.1),
+            new("Elf",    0.45, 1),
+            new("Dwarf",  0.22, 0.99),
+            new("Human",  0.15, 0.95),
+            new("Orc",    0.07, 0.55),
+            new("Goblin", 0.07, 0.5),
+            new("Troll",  0.03, 0.45),
+            new("Balrog", 1,    0.1),
         },
         new("Leg Three")
         {
-            new Encounter("Elf",      0.2,   1),
-            new Encounter("Uruk-Hai", 0.17,  0.4),
-            new Encounter("Goblin",   0.14,  0.5),
-            new Encounter("Human",    0.11,  0.95),
-            new Encounter("Dwarf",    0.105, 0.99),
-            new Encounter("Orc",      0.095, 0.45),
-            new Encounter("Nazgul",   0.08,  0.2),
+            new("Elf",      0.2,   1),
+            new("Uruk-Hai", 0.17,  0.4),
+            new("Goblin",   0.14,  0.5),
+            new("Human",    0.11,  0.95),
+            new("Dwarf",    0.105, 0.99),
+            new("Orc",      0.095, 0.45),
+            new("Nazgul",   0.08,  0.2),
         },
         new("Leg Four")
         {
-            new Encounter("Gollum",          0.45, 0.8),
-            new Encounter("Orc",             0.2,  0.35),
-            new Encounter("Goblin",          0.13, 0.35),
-            new Encounter("Troll",           0.8,  0.40),
-            new Encounter("Spider",          0.7,  0.25),
-            new Encounter("Nazgul",          0.6,  0.15),
-            new Encounter("Sooner Schooner", 0.1,  1),
+            new("Gollum",          0.45, 0.8),
+            new("Orc",             0.2,  0.35),
+            new("Goblin",          0.13, 0.35),
+            new("Troll",           0.8,  0.40),
+            new("Spider",          0.7,  0.25),
+            new("Nazgul",          0.6,  0.15),
+            new("Sooner Schooner", 0.1,  1),
         },
     };
 }
